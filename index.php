5439c7743ec6a93ce09033c31f4a541c8b9bdc64
Ahmad Nadil <88835532+IceTeaXXD@users.noreply.github.com>
2023-09-22 15:55:12 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+if (!session_id()) session_start();
+
+require_once 'app/init.php';
+$app = new App;
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Beasis Wah</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="">
+</head>
+
+<body>
+
+</html>
