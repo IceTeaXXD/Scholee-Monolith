6e89b1c5a808f84a23d56c61ce4f7ebf5e2ef63d
MHEN2606 <matthew.mahendra@gmail.com>
2023-09-25 20:16:46 +0700

    feat: profile api

---
+++ app/controllers/profile.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'app/core/App.php';
+require_once 'app/core/Database.php';
+require_once 'app/models/User.php';
+require_once 'config/config.php';
+class Profile extends Controller
+{
+    public function index()
+    {   
+        $data['judul'] = 'Profile';
+        $data['style'] = "/public/css/profile.css";
+        if (isset($_SESSION['username'])) {
+            $model = new User();
+            $data['user'] = $model->getUser($_SESSION['email'], $_SESSION['role']);
+            $this->view('header/index', $data);
+            $this->view('navbar/index', $data);
+            $this->view('profile/index', $data);
+        } else {
+            header('Location: /login');
+        }
+    }
+}
