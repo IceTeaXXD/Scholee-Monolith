de2b5f954c107941917de3ab5c4148e189a7c65f
Ahmad Nadil <88835532+IceTeaXXD@users.noreply.github.com>
2023-09-25 20:02:54 +0700

    feat: navbar

---
+++ app/views/navbar/index.php
@@ -0,0 +1,58 @@
+<nav>
+    <a href="/">
+        <div class="logo">
+            <span class="logo-text">Beasis Wah</span>
+        </div>
+    </a>
+    <div class="hamburger">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
+    </div>
+    <ul class="nav-links">
+        <?php 
+            if (isset($_SESSION['username'])) { 
+                if($_SESSION['role'] == 'student'){
+        ?>
+            <li><a href="/dashboard">Dashboard</a></li>
+            <li><a href="">Jason</a></li>
+            <li><a href="/scholarships">List Beasiswa</a></li>
+            <li><a href="">Bookmark</a></li>
+            <li><a href="">Cek CV</a></li>
+            <div class="dropdown">
+                <a href="#" class="dropbtn"><?php echo $_SESSION['username']; ?></a>
+                <div class="dropdown-content">
+                    <a href="/profile">Profile</a>
+                    <a href="/api/user/logout.php">Logout</a>
+                </div>
+            </div>
+        <?php   } else if ($_SESSION['role'] == 'admin') {?>
+            <li><a href="/dashboard">Dashboard</a></li>
+            <li><a href="">Admin</a></li>
+            <li><a href="/scholarships">List Beasiswa</a></li>
+            <li><a href="">Bookmark</a></li>
+            <li><a href="">Cek CV</a></li>
+            <div class="dropdown">
+                <a href="#" class="dropbtn"><?php echo $_SESSION['username']; ?></a>
+                <div class="dropdown-content">
+                    <a href="/profile">Profile</a>
+                    <a href="/api/user/logout.php">Logout</a>
+                </div>
+            </div>
+        <?php 
+                } 
+            } else { 
+        ?>
+            <li><a href="/">Home</a></li>
+            <li><a href="">Nadil</a></li>
+            <li><a href="">Our Products</a></li>
+            <li><a href="">About Us</a></li>
+            <li><a href="">Contact Us</a></li>
+            <li><button class="login-button" onclick="redirectToLogin()"><a>Login</a></button></li>
+        <?php 
+            } 
+        ?>
+    </ul>
+</nav>
+
+<script src="/public/js/navbar.js"></script>
