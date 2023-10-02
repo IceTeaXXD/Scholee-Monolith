c47372b70b1375459d1758dfe1a597214682704e
henryanandsr <13521004@std.stei.itb.ac.id>
2023-10-03 01:45:15 +0700

    feat : add ajax addDocument

--- app/views/addDocument/index.php
+++ app/views/addDocument/index.php
@@ -1,14 +1,13 @@
-<div class = "add-beasiswa">
+<div class="add-beasiswa">
     <h1>Add Document</h1>
     <div class="form">
-        
-        <form method = "post" action = "/api/review/add.php" enctype="multipart/form-data">
+        <form action="javascript:;" onsubmit="return submitForm()" enctype="multipart/form-data">
             <label for="file">Upload File</label>
-            <input type="file" name="document" id = "file" required />
+            <input type="file" name="document" id="file" required />
 
-            <button type = "submit" class="save-btn">Tambah Dokumen</button>
-            <a href = "/dashboard" class="cancel-btn">Cancel</a>
+            <button type="submit" class="save-btn">Tambah Dokumen</button>
+            <a href="/dashboard" class="cancel-btn">Cancel</a>
         </form>
-
     </div>
 </div>
+<script src="../../../public/js/addDocument.js"></script>
