11ccfd59b6525b7ede3a6b2c5795c32f32660fd4
Ahmad Nadil <88835532+IceTeaXXD@users.noreply.github.com>
2023-10-03 21:12:56 +0700

    style: footer

--- app/views/footer/index.php
+++ app/views/footer/index.php
@@ -1,13 +1,23 @@
+<link rel="stylesheet" href="/public/css/footer.css">
+<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
 <footer class="footer">
     <div class="footer-logo">
-        <img src="../../../public/image/logo.png" alt="Footer Logo">
+        <img src="/public/image/logo.png" alt="Footer Logo">
+    </div>
+    <div class="footer-social">
+        <a href="https://www.facebook.com/matt.mahen" target="_blank"><img src="/public/image/facebook.svg"></a>
+        <a href="https://twitter.com/itbfess" target="_blank"><img src="/public/image/twitter.svg"></a>
+        <a href="https://www.instagram.com/bernarduswillson/" target="_blank"><img src="/public/image/instagram.svg"></a>
+        <a href="https://www.linkedin.com/in/ahmadnadil/" target="_blank"><img src="/public/image/linkedin.svg"></a>
+        <a href="https://www.youtube.com/@rinaldimunir5569" target="_blank"><img src="/public/image/youtube.svg"></a>
     </div>
     <div class="footer-info">
-        <p>1234 Example Street, City, Country</p>
-        <p>Email: info@example.com</p>
-        <p>Phone: +123-456-7890</p>
+        <p>Wisma Pelangi, Kampung Geulis, Jatinangor, Jawa Barat</p>
+        <p>Email: jasonrivalino1308@gmail.com</p>
+        <p>Phone: +62-877-8193-8298</p>
     </div>
 </footer>
 
 </body>
+
 </html>
