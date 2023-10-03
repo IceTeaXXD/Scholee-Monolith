ccb265e7134154d17153908f910c6a0d4be2143b
Ahmad Nadil <88835532+IceTeaXXD@users.noreply.github.com>
2023-10-03 15:46:25 +0700

    style: login page

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -1,24 +1,35 @@
-<div class="login-page">
-    <div class="form">
-        <form method="post" action = "/api/user/login.php">
-            <h2></i> Login</h2>
-            <input type="text" name="username" placeholder="Email" required />
-            <input type="password" name="password" placeholder="Password" required />
-            <button type="submit" class="btn" href="#">
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                Sign in
+<div class="LoginPage">
+    <div class="LeftColumn">
+        <div class="SectionText">
+            <div class="Top">
+                <div class="SecondaryHeadline">Log In</div>
+            </div>
+        </div>
+        <form class="login-form" method="post" action="/api/user/login.php">
+            <div class="TextField">
+                <div class="LabelAndField">
+                    <div class="Label">Email Address</div>
+                    <div class="Field">
+                        <input type="text" class="Text" placeholder="Enter your email address" name="username">
+                    </div>
+                </div>
+            </div>
+            <div class="TextField">
+                <div class="LabelAndField">
+                    <div class="Label">Password</div>
+                    <div class="Field">
+                        <input type="password" class="Text" placeholder="Enter your password" name="password" id="password">
+                    </div>
+                </div>
+                <div class="Description">It must be a combination of minimum 8 letters, numbers, and symbols.</div>
+            </div>
+            <button type="submit" class="Button">
+                <div class="TextContainer">
+                    <div class="ButtonText">Log In</div>
+                </div>
             </button>
-            <?php
-            if(isset($data['error'])){
-            ?>
-                <div class="alert alert-danger"><?php echo "Login Gagal"; ?></div>
-            <?php
-            }
-            ?>
-            <p class="message">Not registered? <a href="/register">Create an account</a></p>
+            <div class="NoAccountYetSignUp">No account yet? <a href="/register">Sign Up</a></div>
         </form>
     </div>
+    <div class="RightColumn"></div>
 </div>
