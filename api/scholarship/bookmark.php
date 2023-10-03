4d2b31795de5343b6a417464767525fcc49613c2
MHEN2606 <matthew.mahendra@gmail.com>
2023-10-03 11:34:14 +0700

    issue: Bookmark AJAX doesn't work

---
+++ api/scholarship/bookmark.php
@@ -0,0 +1,17 @@
+<?php
+require_once '../../app/core/App.php';
+require_once '../../app/core/Database.php';
+require_once '../../app/models/Scholarship.php';
+require_once '../../config/config.php';
+
+session_start();
+header('Content-Type: application/json');
+
+$model = new Bookmark;
+$prio = 1;
+if ($model->newBookMark($_SESSION['user_id'], $_POST['uis'], $_POST['sid'], $prio)) {
+    echo json_encode(['status' => 'success']);
+} else {
+    echo json_encode(['status' => 'error', 'message' => 'Bookmark creation failed']);
+}
+?>
