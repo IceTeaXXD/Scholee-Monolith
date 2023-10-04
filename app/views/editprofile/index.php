29bd98409f2f8dbae7ab5623fc1137105932ca00
henryanandsr <13521004@std.stei.itb.ac.id>
2023-10-04 10:51:31 +0700

    fix : responsive edit profile

--- app/views/editprofile/index.php
+++ app/views/editprofile/index.php
@@ -4,44 +4,47 @@ $row = mysqli_fetch_array($data['user']);
 <div class="edit-profile">
     <form action="javascript:;" onsubmit="return submitForm()" method="post" class="profile-form" enctype="multipart/form-data">
         <div class="profile-info">
-
-            <img id="profileDisplay" src="/public/image/profiles/<?php echo $row['image'];?>" alt="Profile Image" class="profile-image-display" height="200px">
-            <div class="file-input-wrapper">
-                <label class="file-input-label">
-                    <input type="file" name = "profilepic" id="profileUpload" class="file-input">
-                    <span class="file-input-text">Choose File</span>
-                </label>
+            <div class="grid-items-1">
+                <img id="profileDisplay" src="/public/image/profiles/<?php echo $row['image'];?>" alt="Profile Image" class="profile-image-display" height="200px">
+                <div class="file-input-wrapper">
+                    <label class="file-input-label">
+                        <input type="file" name = "profilepic" id="profileUpload" class="file-input">
+                        <span class="file-input-text">Choose File</span>
+                    </label>
+                </div>
             </div>
-            <label for="fullName">Full Name:</label>
-            <input type="text" id="fullName" name="name" value="<?php echo $row['name'];?>">
-            <?php if($_SESSION['role'] == 'student'){ ?>
-            <label for="gender">University</label>
-            <input type="text" name="university" id="university" value = "<?php echo $row['university'];?>">
-            <label for="dob">Major</label>
-            <input type="text" name="major" id="major" value = "<?= $row['major'];?>">
-            <label for="level">Level:</label>
-            <select id="level" name="level" id="level">
-                <option value="<?php echo $row['level'];?>"><?php echo $row['level'];?></option>
-                <option value="Undergraduate">Undergraduate</option>
-                <option value="Postgraduate">Postgraduate</option>
-                <option value="Doctoral">Doctoral</option>
-            </select>
-            <label for="dob">Street</label>
-            <input type="text" name="street" id="street" value = "<?php echo $row['street'];?>">
-            <label for="dob">City</label>
-            <input type="text" name="city" id = "city"value = "<?php echo $row['city'];?>">
-            <label for="dob">Zipcode</label>
-            <input type="text" name="zipcode" id="zipcode" value = "<?php echo $row['zipcode'];?>">
-            <?php } else if ($_SESSION['role'] == 'admin') { ?>
-            <label for="gender">Organization</label>
-            <input type="text" name="organization" id="organization"value = "<?php echo $row['organization'];?>">
-            <?php } else if ($_SESSION['role'] == 'reviewer'){?>
-            <label for="occupation">Occupation</label>
-            <input type="text" name="occupation" id="occupation"value = "<?php echo $row['occupation'];?>">
-            <?php } ?>
-
-            <button type="submit" class="save-btn">Save Changes</button>
-            <a href="/profile" class="cancel-btn">Cancel</a>
+            <div class="grid-items-2">
+                <label for="fullName">Full Name:</label>
+                <input type="text" id="fullName" name="name" value="<?php echo $row['name'];?>">
+                <?php if($_SESSION['role'] == 'student'){ ?>
+                <label for="gender">University</label>
+                <input type="text" name="university" id="university" value = "<?php echo $row['university'];?>">
+                <label for="dob">Major</label>
+                <input type="text" name="major" id="major" value = "<?= $row['major'];?>">
+                <label for="level">Level:</label>
+                <select id="level" name="level" id="level">
+                    <option value="<?php echo $row['level'];?>"><?php echo $row['level'];?></option>
+                    <option value="Undergraduate">Undergraduate</option>
+                    <option value="Postgraduate">Postgraduate</option>
+                    <option value="Doctoral">Doctoral</option>
+                </select>
+                <label for="dob">Street</label>
+                <input type="text" name="street" id="street" value = "<?php echo $row['street'];?>">
+                <label for="dob">City</label>
+                <input type="text" name="city" id = "city"value = "<?php echo $row['city'];?>">
+                <label for="dob">Zipcode</label>
+                <input type="text" name="zipcode" id="zipcode" value = "<?php echo $row['zipcode'];?>">
+                <?php } else if ($_SESSION['role'] == 'admin') { ?>
+                <label for="gender">Organization</label>
+                <input type="text" name="organization" id="organization"value = "<?php echo $row['organization'];?>">
+                <?php } else if ($_SESSION['role'] == 'reviewer'){?>
+                <label for="occupation">Occupation</label>
+                <input type="text" name="occupation" id="occupation"value = "<?php echo $row['occupation'];?>">
+                <?php } ?>
+    
+                <button type="submit" class="save-btn">Save Changes</button>
+                <a href="/profile" class="cancel-btn">Cancel</a>
+            </div>
         </div>
     </form>
 </div>
--- app/views/profile/index.php
+++ app/views/profile/index.php
@@ -3,18 +3,18 @@ $row = mysqli_fetch_array($data['user']);
 ?>
 <div class="profile-page">
     <div class="profile-info">
-        <img id="profileDisplay" src="/public/image/profiles/<?php echo $row['image'];?>" alt="Profile Image" class="profile-image-display" height="200px">
-        <h2 id="fullNameDisplay">Name: <?php echo $row['name'];?></h2>
+        <img id="profileDisplay" src="/public/image/profiles/<?php echo $row['image']; ?>" alt="Profile Image" class="profile-image-display" height="200px">
+        <h2 id="fullNameDisplay">Name: <?php echo $row['name']; ?></h2>
         <?php
-        if($_SESSION['role'] == 'student'){
+        if ($_SESSION['role'] == 'student') {
         ?>
-        <p class="profile-text">University: <?php echo $row['university'];?></p>
-        <p class="profile-text">Major: <?php echo $row['major'];?></p>
-        <p class="profile-text">Level: <?php echo $row['level'];?></p>
+            <p class="profile-text">University: <?php echo $row['university']; ?></p>
+            <p class="profile-text">Major: <?php echo $row['major']; ?></p>
+            <p class="profile-text">Level: <?php echo $row['level']; ?></p>
         <?php } else if ($_SESSION['role'] == 'admin') { ?>
-        <p class="profile-text">Organization: <?php echo $row['organization'];?></p>
-        <?php } else if ($_SESSION['role'] == 'reviewer'){ ?>
-        <p class="profile-text">Occupation: <?php echo $row['occupation'];?></p>
+            <p class="profile-text">Organization: <?php echo $row['organization']; ?></p>
+        <?php } else if ($_SESSION['role'] == 'reviewer') { ?>
+            <p class="profile-text">Occupation: <?php echo $row['occupation']; ?></p>
         <?php } ?>
         <a href="/profile/edit" class="edit-btn">Edit Profile</a>
     </div>
