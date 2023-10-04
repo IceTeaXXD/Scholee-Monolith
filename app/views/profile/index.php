25e3ce1a26b9a19b32ec27cc7f6166fc2be1c424
henryanandsr <13521004@std.stei.itb.ac.id>
2023-10-04 11:23:23 +0700

    feat : add responsive profile page

--- app/views/profile/index.php
+++ app/views/profile/index.php
@@ -3,19 +3,23 @@ $row = mysqli_fetch_array($data['user']);
 ?>
 <div class="profile-page">
     <div class="profile-info">
-        <img id="profileDisplay" src="/public/image/profiles/<?php echo $row['image']; ?>" alt="Profile Image" class="profile-image-display" height="200px">
-        <h2 id="fullNameDisplay">Name: <?php echo $row['name']; ?></h2>
-        <?php
-        if ($_SESSION['role'] == 'student') {
-        ?>
-            <p class="profile-text">University: <?php echo $row['university']; ?></p>
-            <p class="profile-text">Major: <?php echo $row['major']; ?></p>
-            <p class="profile-text">Level: <?php echo $row['level']; ?></p>
-        <?php } else if ($_SESSION['role'] == 'admin') { ?>
-            <p class="profile-text">Organization: <?php echo $row['organization']; ?></p>
-        <?php } else if ($_SESSION['role'] == 'reviewer') { ?>
-            <p class="profile-text">Occupation: <?php echo $row['occupation']; ?></p>
-        <?php } ?>
-        <a href="/profile/edit" class="edit-btn">Edit Profile</a>
+        <div class="grid-item-profile-1">
+            <img id="profileDisplay" src="/public/image/profiles/<?php echo $row['image']; ?>" alt="Profile Image" class="profile-image-display" height="200px">
+        </div>
+        <div class="grid-item-profile">
+            <h2 id="fullNameDisplay">Name: <?php echo $row['name']; ?></h2>
+            <?php
+            if ($_SESSION['role'] == 'student') {
+            ?>
+                <p class="profile-text">University: <?php echo $row['university']; ?></p>
+                <p class="profile-text">Major: <?php echo $row['major']; ?></p>
+                <p class="profile-text">Level: <?php echo $row['level']; ?></p>
+            <?php } else if ($_SESSION['role'] == 'admin') { ?>
+                <p class="profile-text">Organization: <?php echo $row['organization']; ?></p>
+            <?php } else if ($_SESSION['role'] == 'reviewer') { ?>
+                <p class="profile-text">Occupation: <?php echo $row['occupation']; ?></p>
+            <?php } ?>
+            <a href="/profile/edit" class="edit-btn">Edit Profile</a>
+        </div>
     </div>
 </div>
