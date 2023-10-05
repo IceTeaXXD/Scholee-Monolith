b1bba1d67567a1c83db17a5c39459531994c4f3d
Ahmad Nadil <88835532+IceTeaXXD@users.noreply.github.com>
2023-10-05 23:42:43 +0700

    style: responsivity on kata mereka

--- public/js/home.js
+++ public/js/home.js
@@ -25,12 +25,80 @@ document.addEventListener("DOMContentLoaded", function() {
         } else { // Swiped left
             newScrollPosition = container.scrollLeft + slideWidth;
         }
-
+    
+        // Check if we have reached the end of the container
+        if (newScrollPosition < 0) {
+            // If we have reached the beginning, scroll to the end (last item)
+            newScrollPosition = container.scrollWidth - container.clientWidth;
+        } else if (newScrollPosition >= container.scrollWidth - container.clientWidth) {
+            // If we have reached the end, scroll back to the beginning (first item)
+            newScrollPosition = 0;
+        }
+    
         container.scrollTo({
             left: newScrollPosition,
             behavior: 'smooth'
         });
     }
+    
+
+    function startAutoScroll() {
+        // Scroll to the next item every 2 seconds
+        autoScrollInterval = setInterval(function() {
+            scrollOneItem();
+        }, 2000);
+    }
+
+    function stopAutoScroll() {
+        clearInterval(autoScrollInterval);
+    }
+
+    startAutoScroll(); // Start auto-scroll when the document is ready
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var container = document.querySelector('.grid-container');
+    var items = document.querySelectorAll('.grid-item');
+    var startPos = 0;
+    var endPos = 0;
+    var autoScrollInterval;
+
+    container.addEventListener('touchstart', function(e) {
+        startPos = e.touches[0].clientX;
+        stopAutoScroll(); // stop auto-scroll when user touches the container
+    });
+
+    container.addEventListener('touchend', function(e) {
+        endPos = e.changedTouches[0].clientX;
+        scrollOneItem();
+        startAutoScroll(); // restart auto-scroll after user interaction
+    });
+
+    function scrollOneItem(direction = "left") {
+        var slideWidth = items[0].getBoundingClientRect().width;
+        var newScrollPosition;
+        
+        if (direction === "right" || endPos - startPos > 0) { // Swiped right
+            newScrollPosition = container.scrollLeft - slideWidth;
+        } else { // Swiped left
+            newScrollPosition = container.scrollLeft + slideWidth;
+        }
+    
+        // Check if we have reached the end of the container
+        if (newScrollPosition < 0) {
+            // If we have reached the beginning, scroll to the end (last item)
+            newScrollPosition = container.scrollWidth - container.clientWidth;
+        } else if (newScrollPosition >= container.scrollWidth - container.clientWidth) {
+            // If we have reached the end, scroll back to the beginning (first item)
+            newScrollPosition = 0;
+        }
+    
+        container.scrollTo({
+            left: newScrollPosition,
+            behavior: 'smooth'
+        });
+    }
+    
 
     function startAutoScroll() {
         // Scroll to the next item every 2 seconds
