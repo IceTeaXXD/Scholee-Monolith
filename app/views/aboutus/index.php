cc7f2517677af63eab1459bf69df4d675430ad40
henryanandsr <13521004@std.stei.itb.ac.id>
2023-10-09 10:11:44 +0700

    fix : about us page lighthouse

--- app/views/aboutus/index.php
+++ app/views/aboutus/index.php
@@ -5,7 +5,7 @@
         <li>
             <article class="card" data-visible="false">
                 <div class="card__front flow-content">
-                    <img class="card__img mx-auto" src="https://media.licdn.com/dms/image/C5603AQGPYTUXPvg4iQ/profile-displayphoto-shrink_800_800/0/1629891955062?e=1701907200&v=beta&t=XBX3bDcnHD5A-7hrkbHH-B2SyNWiyCCa-Ac1qZ21xLM" />
+                    <img class="card__img mx-auto" src="https://media.licdn.com/dms/image/C5603AQGPYTUXPvg4iQ/profile-displayphoto-shrink_800_800/0/1629891955062?e=1701907200&v=beta&t=XBX3bDcnHD5A-7hrkbHH-B2SyNWiyCCa-Ac1qZ21xLM" alt="Portrait of Henry Anand Septian R." />
                     <div class="flow-content" data-spacing="sm">
                         <p class="card__name">Henry Anand Septian R.</p>
                         <p class="card__position">13521004</p>
@@ -16,28 +16,29 @@
                     <q>Halo, aku manusia dari Mojokerto, aku rasis, kerjaku adalah membully tiap hari.</q>
                     <ul role="list" class="card__social flex-group">
                         <li>
-                            <a href="https://twitter.com/cclarc8">
+                            <a href="https://twitter.com/cclarc8" aria-label="Henry's Twitter">
                                 <i class="fa-brands fa-twitter "></i>
                             </a>
                         </li>
                         <li>
-                            <a href="https://www.linkedin.com/in/henry-anand-septian-radityo/">
+                            <a href="https://www.linkedin.com/in/henry-anand-septian-radityo/" aria-label="Henry's LinkedIn">
                                 <i class="fa-brands fa-linkedin "></i>
                             </a>
                         </li>
                     </ul>
                 </div>
                 <footer class="card__footer">
-                    <button data-card-controller class="card__toggle">
+                    <button data-card-controller class="card__toggle" aria-label="Toggle card details for Henry Anand Septian R.">
                         <i class="fa-solid fa-plus card__toggle-icon"></i>
                     </button>
                 </footer>
             </article>
         </li>
+        
         <li>
             <article class="card" data-visible="false">
                 <div class="card__front flow-content">
-                    <img class="card__img mx-auto" src="https://media.licdn.com/dms/image/C4D03AQG6Iacmk5bpAQ/profile-displayphoto-shrink_200_200/0/1632934624732?e=1701907200&v=beta&t=GiOJUZhaTMQ2zjHbp4iZ_pZo7j5plmbWfkbV2lzs35M" />
+                    <img class="card__img mx-auto" src="https://media.licdn.com/dms/image/C4D03AQG6Iacmk5bpAQ/profile-displayphoto-shrink_200_200/0/1632934624732?e=1701907200&v=beta&t=GiOJUZhaTMQ2zjHbp4iZ_pZo7j5plmbWfkbV2lzs35M" alt="Portrait of Matthew Mahendra" />
                     <div class="flow-content" data-spacing="sm">
                         <p class="card__name">Matthew Mahendra</p>
                         <p class="card__position">13521007</p>
@@ -48,28 +49,29 @@
                     <q>Sipaling bucin sama 13221002 ke Bandung mulu dahlah gak ngerti lagi.</q>
                     <ul role="list" class="card__social flex-group">
                         <li>
-                            <a href="https://twitter.com/ifmahendra">
+                            <a href="https://twitter.com/ifmahendra" aria-label="Matthew's Twitter">
                                 <i class="fa-brands fa-twitter"></i>
                             </a>
                         </li>
                         <li>
-                            <a href="https://www.linkedin.com/in/matthew-mahendra-117845214/">
+                            <a href="https://www.linkedin.com/in/matthew-mahendra-117845214/" aria-label="Matthew's LinkedIn">
                                 <i class="fa-brands fa-linkedin"></i>
                             </a>
                         </li>
                     </ul>
                 </div>
                 <footer class="card__footer">
-                    <button data-card-controller class="card__toggle">
+                    <button data-card-controller class="card__toggle" aria-label="Toggle card details for Matthew Mahendra">
                         <i class="fa-solid fa-plus card__toggle-icon"></i>
                     </button>
                 </footer>
             </article>
         </li>
+
         <li>
             <article class="card" data-visible="false">
                 <div class="card__front flow-content">
-                    <img class="card__img mx-auto" src="https://avatars.githubusercontent.com/u/88835532?v=4" />
+                    <img class="card__img mx-auto" src="https://avatars.githubusercontent.com/u/88835532?v=4" alt="Portrait of Ahmad Nadil" />
                     <div class="flow-content" data-spacing="sm">
                         <p class="card__name">Ahmad Nadil</p>
                         <p class="card__position">13521024</p>
@@ -80,19 +82,19 @@
                     <q>hah hoh hah hoh saya suka ngoding ngoding is lyfe lyfe is ngoding ha ho ha ho</q>
                     <ul role="list" class="card__social flex-group">
                         <li>
-                            <a href="https://twitter.com/dealdealna">
+                            <a href="https://twitter.com/dealdealna" aria-label="Ahmad's Twitter">
                                 <i class="fa-brands fa-twitter"></i>
                             </a>
                         </li>
                         <li>
-                            <a href="https://www.linkedin.com/in/ahmadnadil/">
+                            <a href="https://www.linkedin.com/in/ahmadnadil/" aria-label="Ahmad's LinkedIn">
                                 <i class="fa-brands fa-linkedin "></i>
                             </a>
                         </li>
                     </ul>
                 </div>
                 <footer class="card__footer">
-                    <button data-card-controller class="card__toggle">
+                    <button data-card-controller class="card__toggle" aria-label="Toggle card details for Ahmad Nadil">
                         <i class="fa-solid fa-plus card__toggle-icon"></i>
                     </button>
                 </footer>
