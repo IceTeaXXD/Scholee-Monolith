380344ed92536327555b4e24e05f70ea8b2f8dd2
MHEN2606 <matthew.mahendra@gmail.com>
2023-11-10 21:32:10 +0700

    feat: Rendering on Submission File

--- public/js/submit.js
+++ public/js/submit.js
@@ -19,4 +19,28 @@ const render = () => {
 const title = document.getElementById("assignment_name");
 const description = document.getElementById("description");
 
+const submitForm = () => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", `http://localhost:5000/api/files/scholarship/${sid}/assignment/${aid}`)
+
+    xhr.onload = () => {
+        const res = JSON.parse(xhr.response);
+        console.log(res.data);
+
+        if (res.status === 'success') {
+
+            title.innerHTML = res.data.assignment_name
+            description.innerHTML = res.data.assignment_description
+
+        }
+    }
+
+    const body = {
+        "uid": uid,
+        "file_path": file_path
+    }
+
+    xhr.send(body)
+}
+
 document.addEventListener("DOMContentLoaded", render);
