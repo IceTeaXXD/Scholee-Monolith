07e8f143329a2be65899844d70d78fadfa4ab008
MHEN2606 <matthew.mahendra@gmail.com>
2023-11-14 16:27:44 +0700

    chore: Update University Stat Info

--- api/university/stats.php
+++ api/university/stats.php
@@ -4,7 +4,26 @@ require_once '../../app/core/Database.php';
 require_once '../../config/config.php';
 
 $db = new Database();
-$query = "SELECT university_id, name, count(*) as count FROM university NATURAL JOIN student GROUP BY university_id";
+$query = "SELECT
+uni.university_id,
+uni.name AS university_name,
+u.name AS applicant_name,
+u.email AS applicant_email,
+counts.applicant_count
+FROM
+university uni
+INNER JOIN student s ON uni.university_id = s.university_id
+INNER JOIN user u ON s.user_id = u.user_id
+INNER JOIN (
+    SELECT
+        university_id,
+        COUNT(*) AS applicant_count
+    FROM
+        student
+    GROUP BY
+        university_id
+) counts ON uni.university_id = counts.university_id";
+
 $stmt = $db->setSTMT($query);
 $result = mysqli_stmt_execute($stmt);
 $row = mysqli_fetch_all(mysqli_stmt_get_result($stmt), MYSQLI_ASSOC);
